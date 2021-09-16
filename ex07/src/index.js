953351818085d287fef0b1e40c14396d74a6da05
Denad Ramic <denad.ramic@gmail.com>
2021-09-16 19:05:13 +0200

    Repository for Day04 - HTML and JS

---
+++ ex07/src/index.js
@@ -0,0 +1,11 @@
+function main(numOne, numTwo) {
+  // only change code below this code
+  var remainder = numOne % numTwo;
+  // only change code above this line
+  return remainder;
+}
+console.log(main(5, 2)); //Change this code
+console.log(main(15, 4)); //Change this code
+console.log(main(102, 25)); //Change this code
+
+module.exports = main;
---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+  // only change code below this line
+  var myFirstName = "Đenad";
+  var myLastName = "Ramić";
+  var myHomeTown = "Sarajevo";
+  // only change code above this line
+  return {
+    myFirstName,
+    myLastName,
+    myHomeTown,
+  };
+}
+
+console.log(main());
+module.exports = main;
